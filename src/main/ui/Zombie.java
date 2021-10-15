0be2350ddee858c3500adfccaee487fe94a31ca4
Netrix <ychengli11065@gmail.com>
2021-10-15 13:39:56 -0700

    Brief introduction of my project

--- src/main/ui/Zombie.java
+++ src/main/ui/Zombie.java
@@ -1,10 +1,42 @@
 package ui;
 
+import model.Map;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.Timer;
+
 public class Zombie {
+
+    private static final int Interval = 10;
+
+    private Map game;
+
+
+    public Zombie() {
+        game =  new Map();
+        addCounter();
+
+    }
+
+
+    //EFFECT: Timer that updates game every Interval millisecond
+    public void addCounter() {
+        Timer t1 = new Timer(Interval, new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent ae) {
+                game.update();
+            }
+        });
+
+        t1.start();
+
+    }
 
 
 
     public static void main(String[] args) {
+        new Zombie();
 
     }
 }
